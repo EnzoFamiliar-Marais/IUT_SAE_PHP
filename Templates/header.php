5aa685024645b9c2b709a1add32d3f7c2c46b49c
Jean-MarcJ <jean-marc.jorite@etu.univ-orleans.fr>
2025-03-02 20:36:17 +0100

    develop propre

--- Templates/header.php
+++
@@ -1,7 +0,0 @@
-<header>
-        <h1>IUTables’O</h1>
-        <nav>
-            <a href="../index.php">Accueil</a>
-            <a href="../Action/resto.php">Les Restos</a>
-        </nav>
-</header>
