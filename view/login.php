3ace35c60b26b13bed0214ebf1cdedca24991fe2
EnzoFamiliar-Marais <enzo.familiar-marais@etu.univ-orleans.fr>
2025-03-05 23:56:39 +0100

    amélioration du style de la page de connexion

--- view/login.php
+++ view/login.php
@@ -6,7 +6,6 @@
     <title>IUTables’O - Resto</title>
     <link rel="stylesheet" href="../static/css/login.css">
     <link rel="stylesheet" href="../static/css/header.css">
-
     
 
     
@@ -19,22 +18,36 @@
 
     <?php
 
+
     if (isset($_SESSION['errorAdd'])) {
-        echo $_SESSION['errorAdd'];
+        echo '<p class="error-message">' . $_SESSION['errorAdd'] . '</p>';
     } else {
         unset($_SESSION['errorAdd']);
     }
-    echo  $_SESSION['errorConnexion'];
-    echo  $_POST['email'];
-    if(isset($_SESSION['errorLogin'])){
-        echo $_SESSION['errorLogin'];
+
+    if (isset($_SESSION['errorConnexion'])) {
+        echo '<p class="error-message">' . $_SESSION['errorConnexion'] . '</p>';
     }
+
+    if (isset($_SESSION['errorLogin'])) {
+        echo '<p class="error-message">' . $_SESSION['errorLogin'] . '</p>';
+    }
+
+
     echo '<div class="container">';
     echo '<h2 class="title">Se connecter</h2>';
     echo '<div class="form-box">';
     echo $form;
     echo '<p class="signup-link"><a href="/?controller=ControlleurRegister&action=view">Créer un compte</a></p>';
     echo '</div>';
+
+    echo '<h2 class="back" ><a href="/?controller=ControlleurHome&action=view">Retour à l\'accueil</a></h2>';
+    echo '</div>';
+
+    require_once 'footer.php';
+    
     ?>
+
+
 </body>
 </html>
