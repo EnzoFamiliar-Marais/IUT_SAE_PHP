8cfd0bd7c3b42819c06bce93fe58dddac1e2cb8b
Romintoo <limaromain8@gmail.com>
2025-03-05 12:03:06 +0100

    tous les utilisateurs peuvent voir les avis, seuls ceux qui sont logs peuvent mettre des avis

---
+++ fichiers/ControlleurDetailResto.php
@@ -0,0 +1,166 @@
+<?php
+namespace Controlleur;
+
+use Auth\DBRestaurant;
+use Auth\DBCritique;
+use Auth\DBAuth;
+use Auth\DBCommune;
+use Auth\DBDepartement;
+use Auth\DBRegion;
+use form\Form;
+use form\type\Link;
+use form\type\Select;
+use form\type\Submit;
+use form\type\Hidden;
+use form\type\Text;
+
+class ControlleurDetailResto extends Controlleur
+{
+   
+    public function view()
+    {
+            
+                
+
+                error_log(print_r($_GET, true));
+                error_log("id_resto : ".$_GET["id"]);
+                $dbRestaurant = new DBRestaurant();
+                $dbCommune = DBCommune::getAllCommunes();
+                $dbRegion = DBRegion::getAllRegions();
+                $dbDepartement = DBDepartement::getAllDepartements();
+                $restaurant =  $dbRestaurant->getRestaurantById($_GET["id"]);
+                
+
+                $commune = null;
+                $region = null;
+                $departement = null;
+        
+                if ($restaurant != null) {
+                    foreach ($dbCommune as $communeItem) {
+                        if ($communeItem["idC"] == $restaurant["idCommune"]) {
+                            error_log("commune : " . $communeItem["nom"]);
+                            $commune = $communeItem["nom"];
+                            $idDepartement = $communeItem["idD"];
+                            break;
+                        }
+                    }foreach ($dbDepartement as $departementItem) {
+                        if ($departementItem["idD"] == $idDepartement) {
+                            error_log("departement : " . $departementItem["nom"]);
+                            $departement = $departementItem["nom"];
+                            $idRegion = $departementItem["idR"];
+                            break;
+                        }
+                    }
+        
+                    foreach ($dbRegion as $regionItem) {
+                        if ($regionItem["idR"] == $idRegion) {
+                            error_log("region : " . $regionItem["nom"]);
+                            $region = $regionItem["nom"];
+                            break;
+                        }
+                    }
+                    $restaurant = $this->renderHoraire($restaurant);
+                }
+
+                
+                error_log(print_r($restaurant, true));
+                $this->render("details_resto.php", [
+                    "restaurant" => $restaurant,
+                    "commune" => $commune,
+                    "region" => $region,
+                    "departement" => $departement,
+                    "formDeconnexion" => $this->getFormDeconnexion(),
+
+                    
+            ]);
+        }
+        
+
+    public function submit()
+    {
+        $auth = new DBAuth();
+        $auth->logout();
+        $this->redirect("ControlleurDetailResto", "view");
+    }
+
+    public function submitAvis(){
+        $dbCritique = new DBCritique();
+        $dbRestaurant = new DBRestaurant();
+        //$dbCritique->addCritique($_POST["note"], $_POST["commentaire"], $user["idU"], $_GET["id"]);
+        $this->redirect("ControlleurDetailResto", "view", $_GET["id"]);
+    }
+
+    public function getFormDeconnexion()
+    {
+        $form = new Form("/?controller=ControlleurDetailResto&action=submit", Form::GET, "home_form");
+        $form->setController("ControlleurDetailResto", "submit");
+        $form->addInput(new Submit("Deconnexion", true, "", ""));
+        return $form;
+    }
+
+    public function getFormDeleteAdmin($id){ 
+        $forms = new Form("/?controller=ControlleurCritique&action=submitDelete", Form::POST, "admin_form");
+        $forms->setController("ControlleurCritique", "submitDelete");
+        $forms->addInput(new Hidden($id,true, "critique_id", "critique_id")); 
+        $forms->addInput(new Submit("Supprimer", true, "", "", ""));
+        
+        return $forms;
+        }
+    
+
+        public function renderHoraire($restaurant) {
+            if ($restaurant["opening_hours"] != null) {
+                $days = ["Mo" => "Lundi", "Tu" => "Mardi", "We" => "Mercredi", "Th" => "Jeudi", "Fr" => "Vendredi", "Sa" => "Samedi", "Su" => "Dimanche"];
+                $hours = explode(';', $restaurant["opening_hours"]);
+                $opening_hours = [];
+        
+                foreach ($hours as $hour) {
+                    list($day_range, $time) = explode(' ', trim($hour), 2);
+                    $day_range_parts = explode('-', $day_range);
+        
+                    if (count($day_range_parts) == 2) {
+                        $start_day = $day_range_parts[0];
+                        $end_day = $day_range_parts[1];
+                        $start_index = array_search($start_day, array_keys($days));
+                        $end_index = array_search($end_day, array_keys($days));
+        
+                        for ($i = $start_index; $i <= $end_index; $i++) {
+                            $day_abbr = array_keys($days)[$i];
+                            $day = $days[$day_abbr];
+                            if (!isset($opening_hours[$day])) {
+                                $opening_hours[$day] = [];
+                            }
+                            $opening_hours[$day][] = $time;
+                        }
+                    } else {
+                        $day = $days[$day_range];
+                        if (!isset($opening_hours[$day])) {
+                            $opening_hours[$day] = [];
+                        }
+                        $opening_hours[$day][] = $time;
+                    }
+                }
+        
+                $restaurant["opening_hours_processed"] = $opening_hours;
+        
+                $today = $days[date('N') - 1]; 
+                $currentTime = date('H:i');
+                $isOpenNow = false;
+        
+                if (isset($restaurant["opening_hours_processed"][$today])) {
+                    foreach ($restaurant["opening_hours_processed"][$today] as $timeRange) {
+                        list($startTime, $endTime) = explode('-', $timeRange);
+                        if ($currentTime >= $startTime && $currentTime <= $endTime) {
+                            $isOpenNow = true;
+                            break;
+                        }
+                    }
+                }
+        
+                $restaurant["is_open_now"] = $isOpenNow;
+            }
+        
+            return $restaurant;
+        }
+
+}
--- view/details_resto.php
+++ view/details_resto.php
@@ -10,8 +10,6 @@
     <link rel="stylesheet" href="../static/css/detail_resto.css">
     <link rel="stylesheet" href="../static/css/modals.css">
     <link rel="stylesheet" href="../static/css/avis.css">
-
-
     <script src="../static/js/modals.js" defer></script>
 </head>
 
@@ -21,7 +19,6 @@
         <nav>
             <a href="?controller=ControlleurHome&action=view">Accueil</a>
             <a href="/?controller=ControlleurResto&action=view">Les Restos</a>
-
             <?php
             if (isset($_SESSION['email'])) {
                 echo $formDeconnexion;
@@ -49,167 +46,71 @@
                 <h2 class="capacity">Capacité : <span class="value"><?php echo $restaurant["capacity"] ?></span></h2>
             <?php endif; ?>
         </div>
-            
+
         <div class="restaurant-description">
-                <div class="restaurant-image">
-                    <img src="../static/img/restobase.jpeg" alt="photo" id="imageResto" />
-                </div>
-                <div class="opening-hours">
-                    <h2>Horaires</h2>
-                    <?php if (isset($restaurant["opening_hours_processed"])): ?>
-                        <p style="color: <?php echo $restaurant["is_open_now"] ? 'green' : 'red'; ?>;">
-                            <?php echo $restaurant["is_open_now"] ? 'Ouvert maintenant' : 'Fermé maintenant'; ?>
-                        </p>
-                        <ul>
-                            <?php 
-                            $daysOfWeek = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
-                            foreach ($daysOfWeek as $day): ?>
-                                <li>
-                                    <span class="day"><?php echo $day; ?></span>
-                                    <span class="time">
-                                        <?php 
-                                        if (isset($restaurant["opening_hours_processed"][$day])) {
-                                            echo implode('<br>', array_map('trim', $restaurant["opening_hours_processed"][$day]));
-                                        } else {
-                                            echo 'Fermé';
-                                        }
-                                        ?>
-                                    </span>
-                                </li>
-                            <?php endforeach; ?>
-                        </ul>
-                    <?php else: ?>
-                        <p>Non renseigné</p>
-                    <?php endif; ?>
-                </div>
+            <div class="restaurant-image">
+                <img src="../static/img/restobase.jpeg" alt="photo" id="imageResto" />
+            </div>
+            <div class="opening-hours">
+                <h2>Horaires</h2>
+                <?php if (isset($restaurant["opening_hours_processed"])): ?>
+                    <p style="color: <?php echo $restaurant["is_open_now"] ? 'green' : 'red'; ?>;">
+                        <?php echo $restaurant["is_open_now"] ? 'Ouvert maintenant' : 'Fermé maintenant'; ?>
+                    </p>
+                    <ul>
+                        <?php 
+                        $daysOfWeek = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
+                        foreach ($daysOfWeek as $day): ?>
+                            <li>
+                                <span class="day"><?php echo $day; ?></span>
+                                <span class="time">
+                                    <?php 
+                                    if (isset($restaurant["opening_hours_processed"][$day])) {
+                                        echo implode('<br>', array_map('trim', $restaurant["opening_hours_processed"][$day]));
+                                    } else {
+                                        echo 'Fermé';
+                                    }
+                                    ?>
+                                </span>
+                            </li>
+                        <?php endforeach; ?>
+                    </ul>
+                <?php else: ?>
+                    <p>Non renseigné</p>
+                <?php endif; ?>
+            </div>
         </div>
+
         <div class="restaurant-caractéristique">
-        <h3>Caractéristiques :</h3>
-        <div class="restaurant-details">
-            <div class="restaurant-info">
-                <div id="detailsGauche">
-                    <p class="label">Type de restaurant : <span class="value"><?php echo $restaurant["type"];  ?></span></p>
-                   
-                    <?php if ($restaurant["brand"] != null): ?>
-                        <p class="label">Marque : <span class="value"><?php echo $restaurant["brand"] ?></span></p>
-                    <?php else: ?>
-                        <p class="label">Marque : <span class="value">Non renseigné</span></p>
-                    <?php endif; ?>
-
-                    <?php if ($restaurant["siret"] != null): ?>
-                        <p class="label">Siret : <span class="value"><?php echo $restaurant["siret"] ?></span></p>
-                    <?php else: ?>
-                        <p class="label">Siret : <span class="value">Non renseigné</span></p>
-                    <?php endif; ?>
-
-
-                    <?php if ($restaurant["telephone"] != null): ?>
-                        <p class="label">Téléphone : <span class="value"><?php echo $restaurant["telephone"] ?></span></p>
-                    <?php else: ?>
-                        <p class="label">Téléphone : <span class="value">Non renseigné</span></p>
-                    <?php endif; ?>
-
-                    <?php if ($restaurant["website"] != null): ?>
-                        <p class="label">Site web : <span class="value"><a href="<?php echo $restaurant["website"]; ?>" target="_blank"><?php echo $restaurant["website"]; ?></a></span></p>
-                    <?php else: ?>
-                        <p class="label">Site web : <span class="value">Non renseigné</span></p>
-                    <?php endif; ?>
-
-                    <?php if ($restaurant["operator"] != null): ?>
-                        <p class="label">Opérateur : <span class="value"><?php echo $restaurant["operator"] ?></span></p>
-                    <?php endif; ?>
-
-                    <?php if ($commune != null): ?>
-                        <p class="label">Commune : <span class="value"><?php echo $commune; ?></span></p>
-                    <?php else: ?>
-                        <p class="label">Commune : <span class="value">Non renseigné</span></p>
-                    <?php endif; ?>
-
-
-                    <?php if ($departement != null): ?>
-                        <p class="label">Département : <span class="value"><?php echo $departement ?></span></p>
-                    <?php else: ?>
-                        <p class="label">Département : <span class="value">Non renseigné</span></p>
-                    <?php endif; ?>
-
-                    <?php if ($region != null): ?>
-                        <p class="label">Région : <span class="value"><?php echo $region ?></span></p>
-                    <?php else: ?>
-                        <p class="label">Région : <span class="value">Non renseigné</span></p>
-                    <?php endif; ?>
-
+            <h3>Caractéristiques :</h3>
+            <div class="restaurant-details">
+                <div class="restaurant-info">
+                    <p class="label">Type de restaurant : <span class="value"><?php echo $restaurant["type"]; ?></span></p>
+                    <p class="label">Marque : <span class="value"><?php echo $restaurant["brand"] ?? 'Non renseigné'; ?></span></p>
+                    <p class="label">Siret : <span class="value"><?php echo $restaurant["siret"] ?? 'Non renseigné'; ?></span></p>
+                    <p class="label">Téléphone : <span class="value"><?php echo $restaurant["telephone"] ?? 'Non renseigné'; ?></span></p>
+                    <p class="label">Site web : <span class="value">
+                        <?php if ($restaurant["website"] != null): ?>
+                            <a href="<?php echo $restaurant["website"]; ?>" target="_blank">Lien</a>
+                        <?php else: ?>
+                            Non renseigné
+                        <?php endif; ?>
+                    </span></p>
+                    <p class="label">Commune : <span class="value"><?php echo $commune ?? 'Non renseigné'; ?></span></p>
+                    <p class="label">Département : <span class="value"><?php echo $departement ?? 'Non renseigné'; ?></span></p>
+                    <p class="label">Région : <span class="value"><?php echo $region ?? 'Non renseigné'; ?></span></p>
                     <p class="label">Regarder sur OSM : <span class="value"><a href="https://www.openstreetmap.org/edit?node=3422189698" target="_blank">Lien OSM</a></span></p>
-                </div>
-
-                <div id="detailsDroite">
-                    <?php if ($restaurant["wheelchair"] == true): ?>
-                        <p class="label">Accessibilité en fauteuil roulant : <span class="value"><?php echo "OUI" ?></span></p>
-                    <?php else: ?>
-                        <p class="label">Accessibilité en fauteuil roulant : <span class="value">Non</span></p>
-                    <?php endif; ?>
-
-                    <?php if ($restaurant["takeaway"] == true): ?>
-                        <p class="label">A Emporter : <span class="value"><?php echo "OUI" ?></span></p>
-                    <?php else: ?>
-                        <p class="label">A Emporter : <span class="value">Non</span></p>
-                    <?php endif; ?>
-
-                    <?php if ($restaurant["delivery"] == true): ?>
-                        <p class="label">Livraison : <span class="value"><?php echo "OUI" ?></span></p>
-                    <?php else: ?>
-                        <p class="label">Livraison : <span class="value">Non</span></p>
-                    <?php endif; ?>
-
-                    <?php if ($restaurant["internet_access"] == true): ?>
-                        <p class="label">Accès internet : <span class="value"><?php echo "OUI" ?></span></p>
-                    <?php else: ?>
-                        <p class="label">Accès internet : <span class="value">Non</span></p>
-                    <?php endif; ?>
-
-                    <?php if ($restaurant["vegetarian"] == true): ?>
-                        <p class="label">Végétarien : <span class="value"><?php echo "OUI" ?></span></p>
-                    <?php else: ?>
-                        <p class="label">Végétarien : <span class="value">Non</span></p>
-                    <?php endif; ?>
-
-                    <?php if ($restaurant["vegan"] == true): ?>
-                        <p class="label">Végan : <span class="value"><?php echo "OUI" ?></span></p>
-                    <?php else: ?>
-                        <p class="label">Végan : <span class="value">Non</span></p>
-                    <?php endif; ?>
-
-                    <?php if ($restaurant["smoking"] == true): ?>
-                        <p class="label">Fumeur : <span class="value"><?php echo "OUI" ?></span></p>
-                    <?php else: ?>
-                        <p class="label">Fumeur : <span class="value">Non</span></p>
-                    <?php endif; ?>
-
-                    <?php if ($restaurant["drive_through"] == true): ?>
-                        <p class="label">Service au volant : <span class="value"><?php echo "OUI" ?></span></p>
-                    <?php else: ?>
-                        <p class="label">Service au volant : <span class="value">Non</span></p>
-                    <?php endif; ?>
-
-                    <?php if ($restaurant["facebook"] != null): ?>
-                        <p class="label">Facebook :
-                            <span class="value">
-                                <a href="<?php echo $restaurant["facebook"]; ?>" target="_blank">
-                                    <img src="../static/img/facebook_logo.png" alt="Facebook" class="facebook-logo">
-                                    <?php echo $restaurant["facebook"]; ?>
-                                </a>
-                            </span>
-                        </p>
-                    <?php else: ?>
-                        <p class="label">Facebook : <span class="value">Non renseigné</span></p>
-                    <?php endif; ?>
                 </div>
             </div>
         </div>
-        </div>
-    </main>
-    <?php if (isset($_SESSION['email'])): ?>
+
         <div class="avisContainer">
-            <button type="button" id="openAvisModal">Donner votre Avis</button>
+            <?php if (isset($_SESSION['email'])): ?>
+                <button type="button" id="openAvisModal">Donner votre Avis</button>
+            <?php else: ?>
+                <p>Vous devez être connecté pour laisser un avis.</p>
+            <?php endif; ?>
+
             <h2>Avis</h2>
             <ul class="avis">
                 <li class="avis-item">
@@ -226,10 +127,8 @@
                 </li>
             </ul>
         </div>
-    <?php else: ?>
-        <p>Vous devez être connecté pour laisser un avis.</p>
-    <?php endif; ?>
-    
+    </main>
+
     <?php require_once 'modals.php'; ?>
     <?php require_once 'footer.php'; ?>
 </body>
