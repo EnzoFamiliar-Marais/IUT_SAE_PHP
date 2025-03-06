536aa6bc2beb1571170704bfb8da3f6771a6a033
Jean-MarcJ <jean-marc.jorite@etu.univ-orleans.fr>
2025-03-06 11:56:54 +0100

    correction deconnexion sur le detail resto

--- Classes/Controlleur/ControlleurDetailResto.php
+++ Classes/Controlleur/ControlleurDetailResto.php
@@ -80,7 +80,7 @@ class ControlleurDetailResto extends Controlleur
     {
         $auth = new DBAuth();
         $auth->logout();
-        $this->redirect("ControlleurDetailResto", "view");
+        $this->redirect("ControlleurDetailResto", "view", $_GET["form_id"]);
     }
 
     public function submitAvis(){
@@ -92,7 +92,7 @@ class ControlleurDetailResto extends Controlleur
 
     public function getFormDeconnexion()
     {
-        $form = new Form("/?controller=ControlleurDetailResto&action=submit", Form::GET, "home_form");
+        $form = new Form("/?controller=ControlleurDetailResto&action=submit", Form::GET, $_GET['id']);
         $form->setController("ControlleurDetailResto", "submit");
         $form->addInput(new Submit("Deconnexion", true, "", ""));
         return $form;
